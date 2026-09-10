d4772b18ef5f7d842ee501221cad5a03e136d517
timbossdap <ng.tc.timtohy@gmail.com>
2026-09-10 15:00:46 +1000

    yuh

--- web/config.js
+++ web/config.js
@@ -2,5 +2,5 @@
 // SUPABASE_URL looks like:      https://xxxxxxxxxxxx.supabase.co
 // SUPABASE_ANON_KEY is the long "anon public" key (safe to expose in client code)
 
-const SUPABASE_URL = "PASTE_YOUR_SUPABASE_URL_HERE";
-const SUPABASE_ANON_KEY = "PASTE_YOUR_SUPABASE_ANON_KEY_HERE";
+const SUPABASE_URL = "https://krhdqranidmepepyuxti.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtyaGRxcmFuaWRtZXBlcHl1eHRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkwMDc1NTMsImV4cCI6MjEwNDU4MzU1M30.-EYb39lgDUfe3j1O64Z1mqEJUiuF-yD6wKbGz8TN6x8";
